c25b54bb553f46109180940a22e53bb1fcd627b6
TestPwst <testingmexico1@gmail.com>
2025-02-20 11:34:04 -0600

    Agregando parametros en init.py

--- __init.py__.py
+++ __init.py__.py
@@ -1,6 +1,6 @@
 import unittest
 
-def load_tests(tests):
+def load_tests(loader, tests, pattern):
     # Filtra archivos que no sean pruebas
     excluded_files = {"__init__.py", "FuncionesGral.py", "VariablesGral.py"}
     filtered_tests = [t for t in tests if t._testMethodName not in excluded_files]
